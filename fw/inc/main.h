6a8c6ae00bafd8485e6cb3ae1bc903968f39ed7f
Jan Horacek <me@apophis.cz>
2023-09-16 18:07:42 +0200

    Tweak alert time (1s).

--- fw/inc/main.h
+++ fw/inc/main.h
@@ -32,7 +32,7 @@ extern volatile Warnings warnings;
 #define DCCON_TIMEOUT_MS 1000
 
 #define BRTEST_NOTEST_MAX_TIME (10) // seconds
-#define ALERT_TIME (3000) // milliseconds
+#define ALERT_TIME (1000) // milliseconds
 
 void set_mode(DCmode);
 void set_relays(bool relay1, bool relay2);
